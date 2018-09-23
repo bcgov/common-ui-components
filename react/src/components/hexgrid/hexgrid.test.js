68e10d33b92cbbb11002012e94dafef04189e8b3
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-22 19:28:41 -0700

    hexgrid unit test

--- react/src/components/hexgrid/hexgrid.test.js
+++ react/src/components/hexgrid/hexgrid.test.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import {shallow} from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Hexgrid from './Hexgrid';
+
+let wrapper;
+
+beforeEach(() => {
+    const items = [1, 2, 3, 5, 6].map(i => <div>{i}</div>);
+  wrapper = shallow(<Hexgrid items={items}/>);
+});
 
 describe('Hexgrid component', () => {
-    it('renders without crashing', () => {
-        expect(shallow(<div>hello</div>));
-    });
+  it('renders without crashing', () => {
+    expect(wrapper);
+  });
 });
