c3f451edc2a483dc9310aecbc44cf42820587060
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2018-09-22 20:08:30 -0700

    hex blocks now render as hexagons

--- react/src/components/hexgrid/Hexgrid.js
+++ react/src/components/hexgrid/Hexgrid.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import classes from './Hexgrid.css';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
@@ -7,14 +8,54 @@ import Hex from './Hex/Hex';
 class Hexgrid extends Component {
   propTypes = {
     items: PropTypes.arrayOf(PropTypes.node).isRequired,
+    hexW: PropTypes.string,
+    gridMW: PropTypes.string,
+    hexStyles: PropTypes.object,
+  };
+
+  defaultProps = {
+    hexW: '200px',
+    gridMW: '1000px',
+    hexStyles: {
+      'border-color': '#eee',
+    },
+  };
+
+  state = {
+    gridOuterWidth: null,
+    gridInnerWidth: null,
+    windowWidth: null,
   };
 
   displayName = 'Hex grid Component [@bcgov/react-ui-components]';
 
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      nextState.outerWidth !== this.state.outerWidth ||
+      nextState.innerWidth !== this.state.innerWidth
+    );
+  }
+
+  windowResized = e => {
+    this.setState({
+      windowWidth: window.innerWidth,
+    });
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.windowResized);
+  }
+
+  componentDidUnmount() {
+    window.removeEventListener('resize', this.windowResized);
+  }
   render() {
+    const style = { ...this.props.hexStyles };
     //  loop over items and wrap in the Hex Component
     const hexs = this.props.items.map(i => (
-      <Hex key={shortid.generate()}>{i}</Hex>
+      <Hex key={shortid.generate()} style={style}>
+        {i}
+      </Hex>
     ));
 
     return (
@@ -25,4 +66,4 @@ class Hexgrid extends Component {
   }
 }
 
-export default Hexgrid;
+export default Radium(Hexgrid);
